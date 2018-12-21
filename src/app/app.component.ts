c31b038d85c765d2a96c02680fbcbf4d0b0e43d4
Helen <alksalena@gmail.com>
2018-12-21 12:44:32 +0700

    added primitive validation template-driven form, bugfix for channel limit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from './services/config.service';
-
 import { Config } from './shared/config';
 
 @Component({
@@ -10,25 +9,38 @@ import { Config } from './shared/config';
 })
 export class AppComponent implements OnInit { 
   tmpConfig: Config;
-  availableChannelBonding: Array<number> = [];
-  availableChannelLimit: Array<number> = [31, 61, 158];
+  availableChannelBonding: Array<number>;
+  availableChannelLimit: Array<number> = [];
   channelLimit: Array<number>;
+  isSubmitForm: boolean = false;
+  isValidForm: boolean = true;
 
-  constructor(private configService: ConfigService) {
-
-  }
+  constructor(private configService: ConfigService) { }
 
   ngOnInit() {
     this.tmpConfig = this.configService.getConfig();
     this.availableChannelBonding = this.configService.getAvailableChannelBonding();
+    this.availableChannelLimit = this.tmpConfig.ChannelBonding ? this.setAvailableRange(this.tmpConfig) : [];
     this.channelLimit = this.tmpConfig.ChannelLimit ? 
                         this.tmpConfig.ChannelLimit.split(',').map(item => parseInt(item, 10)) : [];
   }
 
+  setAvailableRange(value): Array<number> {
+    return this.configService.getAvailableChannelLimit(this.tmpConfig);
+  }
+
+  getAvailableRange(value): void {
+    this.channelLimit = [];
+    this.availableChannelLimit = this.configService.getAvailableChannelLimit(this.tmpConfig);
+  }
+
   onSubmit() {
+    this.isSubmitForm = true;
+    setTimeout(() => this.isSubmitForm = false, 500);
+    this.isValidForm = this.configService.validationForm(this.tmpConfig);
     this.tmpConfig.ChannelLimit = this.channelLimit.toString();
-    if (this.configService.isValidForm(this.tmpConfig)) {
+    if (this.isValidForm) {
       this.configService.setConfig(this.tmpConfig);
-    }    
+    } else return;    
   }
 }
